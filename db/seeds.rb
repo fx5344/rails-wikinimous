a9e4c74e27e3b6ca2975c0164fdbaf5024e6ead5
fx5344 <fox_files01@hotmail.com>
2023-02-21 15:55:03 +0100

    Rails Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Article.create(title: 'The meaning of lif', content: "It's 4!")
+Article.create(title: 'bla', content: "It's bla!")
+Article.create(title: 'blo', content: "It's blo")
+Article.create(title: 'blanc', content: "It's blanc!")
+Article.create(title: 'bleu', content: "It's bleu!")
+Article.create(title: 'black', content: "It's black!")
+Article.create(title: 'blaaaaar', content: "It's blaaaaaar!")
+Article.create(title: 'blooooooor', content: "It's bloooooor!")
